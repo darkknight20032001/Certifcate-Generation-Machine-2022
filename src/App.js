982f53c7a9af306c5c27bdda382a0b5a035d0ae3
darkknight20032001 <84170745+darkknight20032001@users.noreply.github.com>
2022-04-08 04:15:19 +0530

    Done by Rachit Verma

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Certificate from "./Certificate.jpg";
+import { useEffect, useRef, useState } from "react";
+import { exportComponentAsJPEG } from "react-component-export-image";
+import myDatabase from "./Components/myDatabase";
 function App() {
+  const Wrapper = useRef(null);
+  const [Title, setTitle] = useState(``);
+  function DownloadHandler(event) {
+    event.preventDefault();
+    exportComponentAsJPEG(Wrapper);
+    console.log(Wrapper.current);
+  }
+  console.log(myDatabase);
+  const [Name, setName] = useState(``);
+  const [Link, setLink] = useState(``);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="NavBar">
+        <h1>CERTIFICATE GENERATOR</h1>
+        <select
+          onChange={(event) => {
+            setLink(event.target.value);
+            setName(``);
+            setTitle(``);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="">Select the type of Certificate</option>
+          {myDatabase.map((certificates, key) => {
+            return (
+              <option value={certificates.ImageLink}>
+                {certificates.ImageName}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
+      {Link ? (
+        <div className="Meta">
+          <label>
+            Name:
+            <select
+              onChange={(event) => {
+                setTitle(event.target.value);
+              }}
+            >
+              <option value="">{Title ? `${Title}` : `Select`}</option>
+              <option value="None">None</option>
+              <option value="Master">Master</option>
+              <option value="Baby">Baby</option>
+              <option value="Miss">Miss</option>
+              <option value="Mrs.">Mrs.</option>
+              <option value="Mr.">Mr.</option>
+              <option value="Dr.">Dr.</option>
+              <option value="M.L.A.">MLA</option>
+              <option value="M.P.">MP</option>
+              <option value="Advocate">Adv.</option>
+            </select>
+            <input
+              className="EntryList"
+              type="text"
+              placeholder="Enter your name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+              value={Name}
+            />
+          </label>
+        </div>
+      ) : (
+        <div className="Greetings">
+          <h1>🙏NAMASKAR🙏</h1>
+          <p>Please Select Your Certificate from above.</p>
+        </div>
+      )}
+
+      {Name && Title && (
+        <button type="submit" onClick={DownloadHandler}>
+          Download
+        </button>
+      )}
+      <div id="DownloadWrapper">
+        <div id="certificateWrapper" ref={Wrapper}>
+          {Link && Title && Name && (
+            <span>{Title == "None" ? `${Name}` : `${Title} ${Name}`}</span>
+          )}
+          {Link && <img src={Link} />}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/myDatabase.js
@@ -0,0 +1,33 @@
+import React from "react";
+import birth_certificate from "./birth_certificate.jpg";
+import Divorce_Certificates from "./Divorce-Certificates.jpg";
+import marriage_certificate from "./marriage_certificate.jpg";
+import driving_licence from "./driving_licence.jpg";
+import canva_3 from "./canva_3.jpg";
+import canva_4 from "./canva_4.jpg";
+import canva_5 from "./canva_5.jpg";
+import canva_6 from "./canva_6.jpg";
+import canva_7 from "./canva_7.jpg";
+const myDatabase = [
+  {
+    ImageName: "School Student",
+    ImageLink: canva_5,
+  },
+  {
+    ImageName: "Graduate Student",
+    ImageLink: canva_4,
+  },
+  {
+    ImageName: "Employee",
+    ImageLink: canva_3,
+  },
+  {
+    ImageName: "Retirement",
+    ImageLink: canva_6,
+  },
+  {
+    ImageName: "CBFC Certificate",
+    ImageLink: canva_7,
+  },
+];
+export default myDatabase;
